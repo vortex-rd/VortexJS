b6e662690acbe5b2312efdcc72a981e037b513f7
Javier <jlurgo@gmail.com>
2013-03-22 11:43:56 -0300

    el libro se conecta a la red

--- Biblioteca/Libro.js
+++ Biblioteca/Libro.js
@@ -1,3 +1,13 @@
+var FabricaDeLibros = {
+    conectarLibroALaRed : function(nodo, libro){
+        var portal_libro = new NodoPortalBidiMonoFiltro("libro");
+        nodo.conectarCon(portal_libro);
+        portal_libro.conectarCon(nodo); 
+        
+        un_controlador_de_libro = new ControladorDeLibro(libro, portal_libro);
+    }
+}
+
 var Libro = function(cfg){
     this._titulo = cfg.titulo || "";
     this._autor = cfg.autor || "";
@@ -9,5 +19,41 @@ Libro.prototype = {
     },
     autor : function() {
         return this._autor;
+    }
+};
+
+var VistaDeLibro = function(libro, UI){
+    this._ui = UI;
+  
+    this._label_autor = UI.find('#autor_de_libro_encontrado');
+    this._label_titulo = UI.find('#titulo_de_libro_encontrado');
+    
+    this._label_autor.text(libro.autor());
+    this._label_titulo.text(libro.titulo());
+}
+
+VistaDeLibro.prototype = {
+    dibujarEn: function(panel){
+        panel.append(this._ui);
+    }
+};
+
+var ControladorDeLibro = function(un_libro, un_portal){
+    this._libro = un_libro;
+    this._portal = un_portal;   
+    
+    var self = this;
+    this._portal.pedirMensajes(new FiltroAND([new FiltroXClaveValor("tipoDeMensaje", "vortexComm.biblioteca.busquedaDeLibrosPorAutor"),
+                                              new FiltroXClaveValor("autor", un_libro.autor())]),  
+                           function(mensaje){
+                                self.enviarLibro();
+                           });
+}
+    
+ControladorDeLibro.prototype = {
+    enviarLibro : function() {
+        this._portal.enviarMensaje({tipoDeMensaje: "vortexComm.biblioteca.libro", 
+                                    autor:this._libro.autor(),
+                                    titulo: this._libro.titulo()});
     }
 };
