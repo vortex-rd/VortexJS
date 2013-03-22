4a709d4d04fa4200d6add91dc6fe557f0c5a5950
Javier <jlurgo@gmail.com>
2013-03-22 11:39:17 -0300

    vista de biblioteca se conecta a la red independientemente de la biblioteca

--- Biblioteca/Biblioteca.js
+++ Biblioteca/Biblioteca.js
@@ -2,13 +2,10 @@ var FabricaDeBibliotecas = {
     crearBiblioteca : function(){
         return new Biblioteca();
     },
-    crearBibliotecaConectadaALaRed : function(nodo){
-        var portal_biblioteca = new NodoPortalBidi("biblioteca");
-        nodo.conectarCon(portal_biblioteca);
-        portal_biblioteca.conectarCon(nodo); 
-        
+    crearBibliotecaConectadaALaRed : function(router){
+            
         una_biblioteca = new Biblioteca();
-        un_controlador_de_biblioteca = new ControladorDeBiblioteca(una_biblioteca, portal_biblioteca);
+        un_controlador_de_biblioteca = new ControladorDeBiblioteca(una_biblioteca, router);
         
         return una_biblioteca;
     }
@@ -37,54 +34,28 @@ Biblioteca.prototype ={
     }
 };
 
-var VistaDeBiblioteca = function(biblioteca, plantilla){
-    this._biblioteca = biblioteca;
-    this._plantilla = plantilla;
-    this._input_autor_en_alta = plantilla.find('#input_nombre_autor_del_alta_de_libros');
-    this._input_titulo_en_alta = plantilla.find('#input_titulo_del_alta_de_libros');
-    this._boton_alta_de_libro = plantilla.find('#boton_agregar_del_alta_de_libros');
-    
-    var self = this;
-    this._boton_alta_de_libro.click(function(){
-        self._biblioteca.agregarLibro(
-            new Libro({
-                titulo:self._input_titulo_en_alta.val(), 
-                autor:self._input_autor_en_alta.val()
-            })
-        );
-    });
-    
-    this._biblioteca.vista(this); 
-}
 
-VistaDeBiblioteca.prototype = {
-    dibujarEn: function(panel){
-        panel.append(this._plantilla);
-    }
-};
-
-var ControladorDeBiblioteca = function(una_biblioteca, un_portal){
+var ControladorDeBiblioteca = function(una_biblioteca, un_router){
     this._biblioteca = una_biblioteca;
-    this._portal = un_portal;   
-    
-    var self = this;
-    this._portal.pedirMensajes(new FiltroXClaveValor("tipoDeMensaje", "vortexComm.biblioteca.busquedaDeLibrosPorAutor"),  
-                           function(mensaje){
-                                self.onBusquedaPorAutorRecibida(mensaje);
-                           });
-    this._biblioteca.controlador(this);
+    this._router = un_router;  
+    this.start();
 }
     
 ControladorDeBiblioteca.prototype = {
-    onBusquedaPorAutorRecibida:  function (mensaje) {
-        var librosEncontrados = this._biblioteca.buscarLibrosPorAutor(mensaje.autor);
-        var self = this;        
-        librosEncontrados.ForEach(function(l){
-            self.enviarLibro(l);
-        });
-    },    
+    start : function(){
+        this._portal = new NodoPortalBidi("biblioteca");
+        this._router.conectarCon(this._portal);
+        this._portal.conectarCon(this._router);  
+        this._portal.pedirMensajes(new FiltroXClaveValor("tipoDeMensaje", "vortexComm.biblioteca.agregarLibro"),  
+                       this.onMensajeAgregarLibroRecibido.bind(this));
+        this._biblioteca.controlador(this);
+    },
     libroAgregado : function(un_libro) {
         this.enviarLibro(un_libro);
+        FabricaDeLibros.conectarLibroALaRed(this._router, un_libro);
+    },
+    onMensajeAgregarLibroRecibido: function(un_mensaje) {
+        this._biblioteca.agregarLibro(new Libro(un_mensaje));
     },
     enviarLibro : function(un_libro) {
         this._portal.enviarMensaje({tipoDeMensaje: "vortexComm.biblioteca.libro", 
