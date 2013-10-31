ab1e4d442adcfb2cbf7c229ca1786dc69f3b2961
unknown <clurgo@mds-1163.sds_domain_1.local>
2013-10-31 19:09:24 -0300

    refactor de cliente http

--- NodoClienteHTTP.js
+++ NodoClienteHTTP.js
@@ -4,85 +4,92 @@ To view a copy of this licence, visit: http://creativecommons.org/licenses/by/3.
 Project URL: https://sourceforge.net/p/vortexnet
 */
 var NodoClienteHTTP = function (url, intervalo_polling, verbose) {
-    var _this = this;
+    this.url = url;
+    this.intervalo_polling = (intervalo_polling === undefined) ? 1500 : intervalo_polling;
     this.verbose = verbose || false;
-    var idSesion;
-    var intervaloPolling = (intervalo_polling === undefined)? 1500 : intervalo_polling;;
-    var intervaloPedidoIdSesion = 5000;
-    var bandejaSalida = [];
+
+    this.start();
+};
+
+NodoClienteHTTP.prototype.start = function () {
+    this.intervaloPedidoIdSesion = 5000;
+    this.bandejaSalida = [];
     //arranca con un receptor que no hace nada
-    var receptor = {
+    this.receptor = {
         recibirMensaje: function (un_mensaje) { }
     };
 
-    this.recibirMensaje = function (un_mensaje) {
-        bandejaSalida.push(un_mensaje);
-    }
-
-    this.conectarCon = function (un_receptor) {
-        receptor = un_receptor;
-    }
-    function pedirIdSesion() {
-        $.ajax({
-            type: "POST",            
-            url: url + '/create',
-            xhrFields: {
-                withCredentials: false
-            },
-            success: function (responseData, textStatus, jqXHR) {
-                idSesion = responseData;
-                if(_this.verbose)console.log("idSesion:", idSesion);
-                setTimeout(enviarYRecibirMensajes, intervaloPolling);
-            },
-
-            error: function (request, error) {
-                console.log("errorAlPedirSesion:", error);
-                setTimeout(pedirIdSesion, intervaloPedidoIdSesion);
-            }
-        });
-    }
-
-    function enviarYRecibirMensajes() {
-        var bandejaSalidaAux = [];
-        bandejaSalidaAux = bandejaSalidaAux.concat(bandejaSalida);
-        bandejaSalida = [];
-
-        var datosSalida = {
-            "contenidos": bandejaSalidaAux,
-            "proximaEsperaMinima": 0,
-            "proximaEsperaMaxima": 300000
-        };
-        if (bandejaSalidaAux.length > 0) {
-            if(_this.verbose)console.log("enviando:", bandejaSalidaAux);
-        }
-        $.ajax({
-            type: "POST",
-            url: url + '/session/' + idSesion,
-            xhrFields: {
-                withCredentials: false
-            },
-            data: {
-                mensajes_vortex: JSON.stringify(datosSalida)
-            },
-            success: function (responseData, textStatus, jqXHR) {
-                var mensajesRecibidos = $.parseJSON(responseData).contenidos;
-
-                mensajesRecibidos.forEach(function (element, index, array) {
-                    if(_this.verbose)console.log("mensaje recibido:", element);
-                    receptor.recibirMensaje(element);
-                });
-
-                setTimeout(enviarYRecibirMensajes, intervaloPolling);
-            },
-
-            error: function (request, error) {
-                console.log("error Al Enviar/Recibir Mensajes:", error);
-                setTimeout(pedirIdSesion, intervaloPedidoIdSesion);
-                bandejaSalida = bandejaSalida.concat(bandejaSalidaAux);
-            }
-        });
-    }
-
     //pido sesi�n
-    pedirIdSesion();
+    this.pedirIdSesion();
+};
+
+NodoClienteHTTP.prototype.pedirIdSesion() {
+    var _this = this;
+    $.ajax({
+        type: "POST",
+        url: url + '/create',
+        xhrFields: {
+            withCredentials: false
+        },
+        success: function (responseData, textStatus, jqXHR) {
+            idSesion = responseData;
+            if (_this.verbose) console.log("idSesion:", idSesion);
+            setTimeout(function(){_this.enviarYRecibirMensajes;}, _this.intervaloPolling);
+        },
+
+        error: function (request, error) {
+            console.log("errorAlPedirSesion:", error);
+            setTimeout(function(){_this.pedirIdSesion;}, _this.intervaloPedidoIdSesion);
+        }
+    });
+}
+
+NodoClienteHTTP.prototype.enviarYRecibirMensajes = function() {
+    var _this = this;
+    var bandejaSalidaAux = [];
+    bandejaSalidaAux = this.bandejaSalidaAux.concat(bandejaSalida);
+    this.bandejaSalida = [];
+
+    var datosSalida = {
+        "contenidos": bandejaSalidaAux,
+        "proximaEsperaMinima": 0,
+        "proximaEsperaMaxima": 300000
+    };
+    if (bandejaSalidaAux.length > 0) {
+        if (_this.verbose) console.log("enviando:", bandejaSalidaAux);
+    }
+    $.ajax({
+        type: "POST",
+        url: url + '/session/' + idSesion,
+        xhrFields: {
+            withCredentials: false
+        },
+        data: {
+            mensajes_vortex: JSON.stringify(datosSalida)
+        },
+        success: function (responseData, textStatus, jqXHR) {
+            var mensajesRecibidos = $.parseJSON(responseData).contenidos;
+
+            mensajesRecibidos.forEach(function (element, index, array) {
+                if (_this.verbose) console.log("mensaje recibido:", element);
+                _this.receptor.recibirMensaje(element);
+            });
+
+            setTimeout(function(){_this.enviarYRecibirMensajes;}, intervaloPolling);
+        },
+
+        error: function (request, error) {
+            console.log("error Al Enviar/Recibir Mensajes:", error);
+            setTimeout(function(){_this.pedirIdSesion;}, _this.intervaloPedidoIdSesion);
+            _this.bandejaSalida = _this.bandejaSalida.concat(bandejaSalidaAux);
+        }
+    });
+}
+
+NodoClienteHTTP.prototype.recibirMensaje = function (un_mensaje) {
+    this.bandejaSalida.push(un_mensaje);
+}
+
+NodoClienteHTTP.prototype.conectarCon = function (un_receptor) {
+    this.receptor = un_receptor;
 }
