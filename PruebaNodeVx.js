42688486383cdc61fd3a9ff6eb0d2bf688c26fe1
José Miguel Tabuyo <josetabuyo@gmail.com>
2014-03-10 00:13:39 -0300

    el test mejorado, anda perfecto

--- PruebaNodeVx.js
+++ PruebaNodeVx.js
@@ -2,14 +2,19 @@ var vx = require('./vortex').Vortex;
 var FiltroXEjemplo = require("./FiltrosYTransformaciones").FiltroXEjemplo;
 
 vx.start({verbose:true});
+
 vx.conectarPorWebSockets({
     //url:'https://router-vortex.herokuapp.com' 
     url:'http://localhost:3000'
 });   
 
 vx.pedirMensajes({
-    filtro: new FiltroXEjemplo({tipoDeMensaje:"chota"}),
+    filtro: {
+		tipoDeMensaje:"chota"
+	},
     callback: function(mensaje){
         console.log("llego: " + JSON.stringify(mensaje));
     }
 });
+
+
