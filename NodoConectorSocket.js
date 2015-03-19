ec6272c0bd4bb2f7138f53a7b5e99cfeefe38ed3
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-03-19 19:18:17 -0300

    va

--- NodoConectorSocket.js
+++ NodoConectorSocket.js
@@ -4,29 +4,46 @@ To view a copy of this licence, visit: http://creativecommons.org/licenses/by/3.
 Project URL: https://sourceforge.net/p/vortexnet
 */
 
+/*
+Vortex by Vortex Group is licensed under a Creative Commons Reconocimiento 3.0 Unported License.
+To view a copy of this licence, visit: http://creativecommons.org/licenses/by/3.0/
+Project URL: https://sourceforge.net/p/vortexnet
+*/
+
 if(typeof(require) != "undefined"){
     var NodoNulo = require("./NodoNulo").clase;
 }
 
-var NodoConectorSocket = function(socket, verbose){
-    this.socket = socket;
-    this.verbose = verbose||false;
+var NodoConectorSocket = function(opt){
+    this.socket = opt.socket;
+    this.verbose = opt.verbose||false;
+    this.id = opt.id||"anonimo";
+    this.alDesconectar = opt.alDesconectar||function(){};
 	this.vecino = new NodoNulo();
-    this.start();
-};
-
-NodoConectorSocket.prototype.start = function(){
     var _this = this;
     this.socket.on('mensaje_vortex', function (mensaje) {
-        if(this.verbose) console.log("conector recibe mensaje por socket:", mensaje);
+        if(_this.verbose) console.log("conector recibe mensaje por socket:", mensaje);
         _this.vecino.recibirMensaje(mensaje, _this);
     });
+    this.socket.on('disconnect', function () {
+        _this.desconectarDe(_this.vecino);
+    });
+    if(this.verbose) console.log('socket ' + this.id + ' conectado');
 };
 
 NodoConectorSocket.prototype.conectarCon = function(un_vecino){
 	if(this.vecino === un_vecino) return;
     this.vecino = un_vecino;
 	un_vecino.conectarCon(this);
+};
+
+NodoConectorSocket.prototype.desconectarDe = function(un_nodo){
+    this.receptor = new NodoNulo();
+    this.desconectarDe = function(){};
+    
+    un_nodo.desconectarDe(this);
+    if(this.verbose) console.log('socket ' + this.id + ' desconectado');
+    this.alDesconectar();
 };
 
 NodoConectorSocket.prototype.recibirMensaje = function(mensaje){  
