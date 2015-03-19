b77be113a19dd14a65bb5bb55c002763a7a0b446
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-03-19 18:35:07 -0300

    casi listo

---
+++ Vortex.js
@@ -0,0 +1,14 @@
+/*
+Vortex by Vortex Group is licensed under a Creative Commons Reconocimiento 3.0 Unported License.
+To view a copy of this licence, visit: http://creativecommons.org/licenses/by/3.0/
+Project URL: https://sourceforge.net/p/vortexnet
+*/
+
+if(typeof(require) != "undefined"){
+   	exports.Filtros = require("./Filtros");
+    exports.NodoMultiplexor = require("./NodoMultiplexor").clase;
+    exports.NodoRouter = require("./NodoRouter").clase;
+    exports.NodoConectorSocket = require("./NodoConectorSocket").clase;    
+    //exports.NodoClienteHTTP =  require("./NodoClienteHTTP").clase;    
+    exports.NodoConectorHttpServer = require("./NodoConectorHttpServer").clase;   
+}
